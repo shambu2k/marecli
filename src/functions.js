dbfa7287d2aee7de0c35670e3b43c2dafd71ca6c
shambu2k <sidharthshambu00@gmail.com>
2023-07-19 13:11:24 +0530

    fix: update tags string replace, use silent true

--- src/functions.js
+++ src/functions.js
@@ -18,9 +18,9 @@ export const createBranch = async (inquirer) => {
   ]);
   const newBranch = `${branchType}/${jiraID}`;
 
-  shell.exec(`git checkout ${developmentBranch} > /dev/null 2>&1`);
-  shell.exec(`git pull origin ${developmentBranch}> /dev/null 2>&1`);
-  shell.exec(`git checkout -b ${newBranch} > /dev/null 2>&1`);
+  shell.exec(`git checkout ${developmentBranch}`, { silent: true });
+  shell.exec(`git pull origin ${developmentBranch}`, { silent: true });
+  shell.exec(`git checkout -b ${newBranch}`, { silent: true });
 };
 
 export const raiseRelease = async (inquirer) => {
@@ -33,15 +33,15 @@ export const raiseRelease = async (inquirer) => {
   const newBranch = `${developmentBranch}-release-${CURRENT_DATE_DDMMYY()}`;
 
   colorLog(`Syncing ${developmentBranch}`, "green");
-  shell.exec(`git checkout ${developmentBranch} > /dev/null 2>&1`);
-  shell.exec(`git pull origin ${developmentBranch} > /dev/null 2>&1`);
+  shell.exec(`git checkout ${developmentBranch}`, { silent: true });
+  shell.exec(`git pull origin ${developmentBranch}`, { silent: true });
 
   colorLog(`Syncing ${releaseBranch}`, "green");
-  shell.exec(`git checkout ${releaseBranch} > /dev/null 2>&1`);
-  shell.exec(`git pull origin ${releaseBranch} > /dev/null 2>&1`);
+  shell.exec(`git checkout ${releaseBranch}`, { silent: true });
+  shell.exec(`git pull origin ${releaseBranch}`, { silent: true });
 
   colorLog(`Merging ${developmentBranch} into ${newBranch}`, "green");
-  shell.exec(`git checkout -b ${newBranch} > /dev/null 2>&1`);
+  shell.exec(`git checkout -b ${newBranch}`, { silent: true });
   shell.exec(`git merge ${developmentBranch}`);
 
   const conflicts = shell.exec("git diff --name-only --diff-filter=U", {
@@ -75,26 +75,37 @@ export const raiseUpdateTag = async (inquirer) => {
   const updatTagBranch = `updateTags-${jiraID}-${CURRENT_DATE_DDMMYY()}`;
   const environmentsList = environments.split(",").map((env) => env.trim());
   const newTagTrimmed = newTag.trim();
+  const prefix = releaseBranch.split("/")[1].trim() + ".";
 
   colorLog(`Syncing ${releaseBranch}`, "green");
-  shell.exec(`git checkout ${releaseBranch} > /dev/null 2>&1`);
-  shell.exec(`git pull origin ${releaseBranch} > /dev/null 2>&1`);
+  shell.exec(`git checkout ${releaseBranch}`, { silent: true });
+  shell.exec(`git pull origin ${releaseBranch}`, { silent: true });
 
   colorLog(`Creating ${updatTagBranch} from ${releaseBranch}`, "green");
-  shell.exec(`git checkout -b ${updatTagBranch} > /dev/null 2>&1`);
+  shell.exec(`git checkout -b ${updatTagBranch}`, { silent: true });
 
-  const prefix = releaseBranch.split("/")[1].trim();
   for (let environment of environmentsList) {
     colorLog(`Updating ${environment} tags to ${newTagTrimmed}`, "green");
+
+    // Finding old tags
+    const result = shell.exec(
+      `awk -F'${prefix}' '{print $2}' ${k8sPath}/${environment}/deployment.yaml | tr -d '\\n'`,
+      { silent: true }
+    );
+    const getOldTagsResult = result.stdout.trim();
+    const oldTagTrimmed = getOldTagsResult.match(/^[0-9]+/)?.[0] || "";
+
+    // Updating to new tags
     shell.exec(
-      `LC_ALL=en_US.UTF-8 find ${k8sPath}/${environment} -type f -name 'deployment.yaml' -exec perl -pi -e 's/${prefix}\\.\\K\\S+/${newTagTrimmed}"/g' {}  \\; > /dev/null 2>&1`
+      `find ${k8sPath} -type f -name 'deployment.yaml' -exec perl -pi -e 's/${oldTagTrimmed}/${newTagTrimmed}/g' {} \\;`,
+      { silent: true }
     );
   }
 
   colorLog(`Adding all modified deployment.yaml files to git...`, "green");
-  shell.exec(
-    `git add $(git diff --name-only | grep 'deployment.yaml') > /dev/null 2>&1`
-  );
+  shell.exec(`git add $(git diff --name-only | grep 'deployment.yaml')`, {
+    silent: true,
+  });
 
   colorLog(`Committing changes...`, "green");
   shell.exec(
@@ -120,15 +131,15 @@ export const raiseBackmerge = async (inquirer) => {
   const newBranch = `${developmentBranch}-backmerge-${CURRENT_DATE_DDMMYY()}`;
 
   colorLog(`Syncing ${releaseBranch}`, "green");
-  shell.exec(`git checkout ${releaseBranch} > /dev/null 2>&1`);
-  shell.exec(`git pull origin ${releaseBranch} > /dev/null 2>&1`);
+  shell.exec(`git checkout ${releaseBranch}`, { silent: true });
+  shell.exec(`git pull origin ${releaseBranch}`, { silent: true });
 
   colorLog(`Syncing ${developmentBranch}`, "green");
-  shell.exec(`git checkout ${developmentBranch} > /dev/null 2>&1`);
-  shell.exec(`git pull origin ${developmentBranch} > /dev/null 2>&1`);
+  shell.exec(`git checkout ${developmentBranch}`, { silent: true });
+  shell.exec(`git pull origin ${developmentBranch}`, { silent: true });
 
   colorLog(`Merging ${releaseBranch} into ${newBranch}`, "green");
-  shell.exec(`git checkout -b ${newBranch} > /dev/null 2>&1`);
+  shell.exec(`git checkout -b ${newBranch}`, { silent: true });
   shell.exec(`git merge ${releaseBranch}`);
 
   const conflicts = shell.exec("git diff --name-only --diff-filter=U", {
@@ -164,13 +175,14 @@ export const syncCurrentBranch = async (inquirer) => {
       currentBranch = stdout.trim();
 
       colorLog(`Syncing ${syncFromBranch}`, "green");
-      shell.exec(
-        `git checkout ${syncFromBranch} > /dev/null 2>&1 && git pull > /dev/null 2>&1`
-      );
+      shell.exec(`git checkout ${syncFromBranch} && git pull`, {
+        silent: true,
+      });
 
       colorLog(`Merging ${syncFromBranch} to ${currentBranch}`, "green");
       shell.exec(
-        `git checkout ${currentBranch} > /dev/null 2>&1 && git merge ${syncFromBranch}`
+        `git checkout ${currentBranch} && git merge ${syncFromBranch}`,
+        { silent: true }
       );
     } else {
       console.error("Failed to retrieve the Git branch:", stderr);
